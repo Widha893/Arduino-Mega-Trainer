e48c77ba11961248b8f1123e3de1f18602791f19
alrazzh91 <nazzauraaizachacha@gmail.com>
2023-11-22 14:44:47 +0700

    converting ADC and dB

--- Sound_sensor/Soundsensor/Soundsensor.ino
+++ Sound_sensor/Soundsensor/Soundsensor.ino
@@ -4,27 +4,38 @@
 #define sound_pin A1  
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-float sound_val;
-
+float sound_val, voltage, dB;
+float refVoltage = 5.0;
 
 void lcd_setup(){
-  lcd.init();                      // Inisialisasi LCD
-  lcd.backlight();
+ lcd.init();                      // Inisialisasi LCD
+ lcd.backlight();
 }
 
 void setup() {
-  pinMode (sound_pin, INPUT);
+ pinMode (sound_pin, INPUT);
   Serial.begin(9600);
-  lcd_setup();
+ lcd_setup();
 }
 
 void read_sound()
 {
   sound_val = analogRead(sound_pin); 
-  Serial.println(sound_val);
-    lcd.setCursor(0,0);
-    lcd.print("ADC: ");
-    lcd.print(sound_val);
+  voltage = sound_val * (refVoltage / 1023.0);
+  dB = 20 * log10(voltage);
+
+  Serial.print("Sound value: ");
+  Serial.print(sound_val);
+  Serial.print(" ");
+  Serial.print("Voltage: ");
+  Serial.print(voltage);
+  Serial.print(" ");
+  Serial.print("Decibel: ");
+  Serial.println(dB);
+  
+   lcd.setCursor(0,0);
+   lcd.print("ADC: ");
+   lcd.print(sound_val);
 }
 
 void loop() {
